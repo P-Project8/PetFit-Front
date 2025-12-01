aa9ce2d1fc2d753bb8799ec60afacc7b1b04d693
Batrnan <tjdwo2323@naver.com>
2025-12-01 11:34:03 +0900

    feat: 메인 페이지, 헤더, 네비게이션바, 카테고리 페이지 구현

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router';
+import './index.css';
+import App from './App';
+import RootLayout from './layout';
+import CategoryPage from './pages/CategoryPage';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route element={<RootLayout />}>
+          <Route index element={<App />} />
+          <Route path="/category/:categoryId" element={<CategoryPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
